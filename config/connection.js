31bb1fb216a5dec33254fa9028bcb4c579a3ea22
Brian Graf <brian.g.graf@gmail.com>
2019-12-05 13:13:38 -0700

    fix typo

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
 const mysql = require("mysql");
 
+let connection;
+
 if (process.env.JAWSDB_URL) {
-    connect = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
         host: "localhost",
