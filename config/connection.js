5bc237ba694d8c13baf5f5ba63c3a96e0c67bb29
Brian Graf <brian.g.graf@gmail.com>
2019-12-05 13:06:35 -0700

    add env sql variable

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 const mysql = require("mysql");
 
-connection = mysql.createConnection({
-    host : "localhost",
-    user : "root",
-    password : "root",
-    database : "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connect = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+}
+
 
 connection.connect();
 
